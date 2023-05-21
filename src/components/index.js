3ff91b635a8ec6f53ebcbb845b8510162dacd211
Lucas Lorenzo Pena <lucaslpena@gmail.com>
2023-05-21 17:57:15 +0200

    deploy 2

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Contact from "./Contact";
+import { ContentWrapper } from "./ContentWrapper";
+import Info from "./Info";
+import { LanguageToggle } from "./LanguageToggle";
+import LogoTitle from "./Logo";
+
+export { Contact, ContentWrapper, Info, LanguageToggle, LogoTitle };
