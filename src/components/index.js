e7df9a495e0bb248a1897383021fe3111fdddebc
Lucas Lorenzo Pena <lucaslpena@gmail.com>
2023-05-21 17:50:40 +0200

    build

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-import { ContentWrapper } from "./ContentWrapper";
-import { LanguageToggle } from "./LanguageToggle";
-import LogoTitle from "./Logo";
-export { ContentWrapper, LanguageToggle, LogoTitle };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import Head from "next/head";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.scss";
-import { LogoTitle, ContentWrapper, LanguageToggle } from "@/components";
+import styles from "../styles/Home.module.scss";
+import { LogoTitle, ContentWrapper, LanguageToggle } from "../components";
 import { motion, useTransform } from "framer-motion";
 import useMouse from "@react-hook/mouse-position";
 import {
